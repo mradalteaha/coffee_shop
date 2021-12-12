fcbcf596ca6f980727442967f21f1ef60e33798b
moradteaha <73730311+moradteaha@users.noreply.github.com>
2021-12-12 21:40:56 +0200

    changes in the table reservation and adding the java script

---
+++ coffee shop/Scripts/myFunctions/script.js
@@ -0,0 +1,58 @@
+﻿
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+function myFunction() {
+
+    alert("testing button");
+
+    return true
+}
+function Add() {
+    const menu = document.getElementById("menu-box")
+    menu.innerHTML += `
+    <div class="box">
+    <img src="images/menu-2.png" alt="">
+    <h3>tasty and healty</h3>
+    <div class="price">$15.99 <span>20.99</span></div>
+    <a href="#" class="btn">add to cart</a>
+</div>
+`
+
+}
+
+
+
+function myFunction() {
+
+    alert("clicked");
+    return true;
+}
