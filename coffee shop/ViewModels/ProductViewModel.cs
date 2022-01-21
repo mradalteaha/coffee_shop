545fd011dfab930c88ecc752c347b9a2b4d8f5a6
Abdalbaset10 <74993448+Abdalbaset10@users.noreply.github.com>
2022-01-21 02:49:03 +0200

    product view model

---
+++ coffee shop/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using coffee_shop.Models;
+namespace coffee_shop.ViewModels
+{
+    public class ProductViewModel
+    {
+        public product myprod { get; set; }
+
+        public List<product> products { get; set; }
+    }
+}
